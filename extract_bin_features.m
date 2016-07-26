f2d71551110a29fb8b7d56277b83a5d6ff455440
Ben <bdpowell@email.wm.edu>
2016-07-26 14:02:33 -0400

    Split accelerometer channels for analysis instead of summing them. The tests seemed to be a bit more accurate...

--- extract_bin_features.m
+++ extract_bin_features.m
@@ -1,36 +1,31 @@
-function [ means, stds, rmss, daytime] = extract_bin_features( arr, dates, inds )
+function [ means, stds, rmss, daytime] = extract_bin_features( accel, times, inds )
 % Get mean, std. dev, energy, and RMS for each input bin.
 % These are used later for machine learning.
 
-means = zeros(length(inds), 1);
+means = zeros(length(inds), 3);
 stds = means;
 rmss = means;
-daytime = logical(means);
+daytime = false(length(inds),1);
 
+disp('Binify:');
+tic
 bins = cell(length(inds),1);
 for i=1:length(inds)
-    bins{i} = {single(arr(inds(i,1):inds(i,2))), dates(inds(i,1))};
+    bins{i} = {single(accel(inds(i,1):inds(i,2),:)), times(inds(i,1))};
 end
+toc
 
-parfor i=1:length(bins)
+disp('Stat analysis:');
+tic
+for i=1:length(bins)
    bin = bins{i}{1};
-   means(i) = sum(mean(bin)); 
-   stds(i)  = sum(std(bin)); 
-   rmss(i)  = sum(rms(bin));
+   means(i,:) = mean(bin); 
+   stds(i,:)  = std(bin); 
+   rmss(i,:)  = rms(bin);
    
    hr = hour(bins{i}{2});
    daytime(i) = (hr > 8) & (hr < 16);
 end
-
-nighttime = ~daytime;
-
-hold on
-scatter3(means(daytime), stds(daytime), rmss(daytime), 3, 'r');
-scatter3(means(nighttime), stds(nighttime), rmss(nighttime), 3, 'b');
-xlabel('Mean');
-ylabel('Std. Dev');
-zlabel('RMS');
-title('Statistical Measures of Each Bin');
-hold off
+toc
 
 end
